2d5c15531dc96308fb250f478c126480184371be
ChuYong <jakgon@naver.com>
2023-06-09 19:49:32 +0900

    feat: 웹뷰 위아래 패딩

--- lib/view/feature/webview_screen/full_webview_screen.dart
+++ lib/view/feature/webview_screen/full_webview_screen.dart
@@ -1,5 +1,7 @@
 import 'package:badsound_counter_app/presenter/feature/webview_screen/webview_screen_state.dart';
 import 'package:badsound_counter_app/presenter/feature/webview_screen/webview_screen_store.dart';
+import 'package:badsound_counter_app/view/designsystem/theme/base_color.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
@@ -14,8 +16,15 @@ class FullWebViewScreen extends BaseView<FullWebViewScreen, WebViewScreenAction,
 
   @override
   Widget render(BuildContext context, WebViewScreenAction action, WebViewScreenState state) {
-    return WebViewWidget(
-      controller: action.webViewController,
+    return Container(
+      width: double.infinity,
+      height: double.infinity,
+      color: BaseColor.warmGray200,
+      child: SafeArea(
+        child: WebViewWidget(
+          controller: action.webViewController,
+        ),
+      )
     );
   }
 
